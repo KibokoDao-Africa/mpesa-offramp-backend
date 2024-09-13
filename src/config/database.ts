5168762305b8a51ad5fee6837d82f1966003e6be
timothyAgevi <timothyagevi@gmail.com>
2024-09-13 11:25:10 +0300

    update configurations

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
-import * as dotenv from 'dotenv';
-dotenv.config();
+// import * as dotenv from 'dotenv';
+// dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL as string, 
   {
