c91996bccaa88d851184e4181f819ac61e912a13
timothyAgevi <timothyagevi@gmail.com>
2024-09-29 00:57:00 +0300

    bind server to an address and convert port to type number

--- server.ts
+++ server.ts
@@ -1,19 +1,16 @@
 import "dotenv/config";
 import app from './src/app';
-import  sequelize  from './src/config/database';
+import sequelize from './src/config/database';
 
-// dotenv.config()
 console.log("hello world 3");
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || '3000', 10);
 
 const startServer = async () => {
   try {
     await sequelize.sync();
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server is running on port ${PORT}`);
-
     });
   } catch (error) {
     console.error('Failed to start server:', error);
