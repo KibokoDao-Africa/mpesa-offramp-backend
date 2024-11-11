bf5261d52a682844e0eaaa79740a716cc05be65b
Felix Orinda <forinda82@gmail.com>
2024-11-11 17:58:15 +0300

    Fix auto exit server on dev run

--- server.ts
+++ server.ts
@@ -1,20 +1,23 @@
 import "dotenv/config";
 import app from './src/app';
 import sequelize from './src/config/database';
+import { createServer } from "http";
 
-console.log("hello world 3");
+// console.log("hello world 3");
 
 const PORT = parseInt(process.env.PORT || '8080', 10);
 
-const startServer = async () => {
-  try {
-    await sequelize.sync();
-    app.listen(PORT, "0.0.0.0", () => {
+const startServer = () => {
+const server = createServer(app);
+  sequelize.sync().then(() => {
+    server.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
+    })
+  }).catch((error) => {
     console.error('Failed to start server:', error);
   }
-};
+  )
+}
+
 
 startServer();
